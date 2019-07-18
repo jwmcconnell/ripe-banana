df9e3787ba83a2dfe2d5a898bdd7e98133b51555
Jackmc <jackmc1122@gmail.com>
2019-07-18 15:20:10 -0700

    Update GET reviewer by id to send the reviews they have made with the request.

--- test/reviewers.test.js
+++ test/reviewers.test.js
@@ -2,7 +2,49 @@ const { getReviewer } = require('./dataHelpers');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const Reviewer = require('../lib/models/Reviewer');
+const Review = require('../lib/models/Review');
+const Actor = require('../lib/models/Actor');
+const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
+
 describe('reviewer routes', () => {
+
+  let reviewer;
+  let film;
+  beforeEach(async() => {
+    const actors = await Actor.create([{ name: 'somename' }, { name: 'othername' }]);
+    const studio = await Studio.create({
+      name: 'studio-name',
+      address: {
+        city: 'somecity',
+        state: 'somestate',
+        country: 'somecountry'
+      }
+    });
+    film = await Film.create({
+      title: 'Crazy Film',
+      studio: studio._id,
+      released: 2014,
+      cast: [{ actor: actors[0]._id }, { actor: actors[1]._id }]
+    });
+    reviewer = await Reviewer.create({ name: 'jack-reviewer', company: 'jacks-reviews' });
+    await Review.create([
+      {
+        rating: 5,
+        reviewer: reviewer._id,
+        review: 'Simply amazing.',
+        film: film._id
+      },
+      {
+        rating: 3,
+        reviewer: reviewer._id,
+        review: 'Enjoyable',
+        film: film._id
+      }
+    ]);
+  });
+
   it('creates and returns a reviewer', () => {
     return request(app)
       .post('/api/v1/reviewers')
@@ -34,15 +76,24 @@ describe('reviewer routes', () => {
   });
 
   it('returns a reviewer by their id', async() => {
-    const { _id, name, company } = await getReviewer();
+    // const { _id, name, company } = await getReviewer();
     return request(app)
-      .get(`/api/v1/reviewers/${_id}`)
+      .get(`/api/v1/reviewers/${reviewer._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          _id,
-          name,
-          company,
-          __v: 0
+          _id: reviewer._id.toString(),
+          name: reviewer.name,
+          company: reviewer.company,
+          reviews: expect.any(Array)
+        });
+        expect(res.body.reviews[0]).toEqual({
+          _id: expect.any(String),
+          rating: expect.any(Number),
+          review: expect.any(String),
+          film: {
+            _id: expect.any(String),
+            title: expect.any(String)
+          }
         });
       });
   });
